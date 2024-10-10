15312f3f2f101b0e6fb2cdf356c9b2b2e4af9f3a
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2024-10-10 15:25:55 +0530

    Initial Commit

---
+++ UnitTester/Model/Semester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTester.Model
+{
+    public class Semester
+    {
+        // =========================================================== Properties and Fields
+
+        public int SemesterID { get; set; }               // Primary Key for Indexing, This does not Reflect Semester Number
+
+        public int Level { get; set; }                    // The level the Semester Belongs to
+        public int SemesterCode { get; set; }                    // The Real Semester Number
+        
+        
+        public string? SemesterDescription { get; set; }
+        public int CalenderYear { get; set; }   // The calender year the semester was faced, 0000 to default
+
+        public Dictionary<int, Grade> SubjectScores { get; set; } = new Dictionary<int, Grade>(); // Subject ID and GPV Obtained
+    }
+}
